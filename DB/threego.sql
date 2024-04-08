834ac4c44260b301d8e20d960753c3f62354a90a
Leo-Dietrich <leonardo.dietrich@sptech.school>
2024-04-08 20:50:40 -0300

    Adicionada coluna 'sobrenome' à tabela usuário

--- DB/threego.sql
+++ DB/threego.sql
@@ -4,14 +4,14 @@ use threego;
 
 create table usuario (
 id int primary key auto_increment,
-nome varchar(50) not null,
+nome varchar(35) not null,
+sobrenome varchar(45),
 funcao varchar (40), 
 email varchar(70) not null unique ,
+	constraint chkEmail check (email like ('%@%')),
+senha varchar(40),
 cpf varchar (14) not null unique, 
-telefone varchar(16)not null, 
-senha varchar(40));
-
-alter table usuario add constraint chkEmail check (email like('%@%'));
+telefone varchar(16)not null);
 
 desc usuario;
 
@@ -29,15 +29,13 @@ fkSensor int,
 	constraint fkCamaraSensor foreign key (fkSensor)
 		references sensor(idSensor));
         
-insert into usuario (nome,email, cpf, telefone,senha) values 
-('Joao Silva', 'jotinhasilva@sptech.school', '123.456.789-10','5511999998888','senhasecreta');
+insert into usuario (nome, sobrenome, email, senha, cpf, telefone) values 
+('Joao', 'Silva', 'jotinhasilva@sptech.school','senhasecreta', '123.456.789-10','5511999998888');
 
 insert into sensor(temperatura, humidade) values
 (23.4, 35);
 
 insert into camara (linha, coluna, fkSensor) values
 ('A', '12', 1);
- 
-        
-        
 
+select*from usuario;
