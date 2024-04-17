b89aa0953233772c0583e3409ab85322e736e304
Leo-Dietrich <leonardo.dietrich@sptech.school>
2024-04-17 16:05:15 -0300

    Banco de dados atualizado para inserção com o arduino

--- DB/threego.sql
+++ DB/threego.sql
@@ -2,42 +2,35 @@ create database threego;
 
 use threego;
 
-create table usuario (
-id int primary key auto_increment,
-nome varchar(50) not null,
-funcao varchar (40), 
+create table representante (
+idRep int primary key auto_increment,
+nomeRep varchar(50) not null,
 email varchar(70) not null unique ,
-cpf varchar (14) not null unique, 
+	constraint chkEmail check (email like('%@%')),
 telefone varchar(16)not null, 
 senha varchar(40));
 
-alter table usuario add constraint chkEmail check (email like('%@%'));
+create table empresa (
+idEmpresa int primary key auto_increment,
+razaoSoc varchar(45) not null,
+CNPJ char(14) not null,
+fkRepresentante int not null,
+	constraint fkEmpresaRep foreign key (fkRepresentante)
+		references representante (idRep));
 
-desc usuario;
-
-create table sensor(
+create table dados(
 idSensor int primary key auto_increment,
-temperatura float,
-humidade int,
+dht11Temperatura double,
+dht11Umidade double,
 datahora datetime default current_timestamp);
 
 create table camara(
 idCamara int primary key auto_increment,
 linha char(1),
 coluna varchar(2),
-fkSensor int,
-	constraint fkCamaraSensor foreign key (fkSensor)
-		references sensor(idSensor));
-        
-insert into usuario (nome,email, cpf, telefone,senha) values 
-('Joao Silva', 'jotinhasilva@sptech.school', '123.456.789-10','5511999998888','senhasecreta');
-
-insert into sensor(temperatura, humidade) values
-(23.4, 35);
-
-insert into camara (linha, coluna, fkSensor) values
-('A', '12', 1);
- 
+fkDados int,
+	constraint fkCamaraDados foreign key (fkDados)
+		references dados(idSensor));
         
         
 
