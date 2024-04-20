22f8865d6ca598c74cf9aee0a0731f1d7a180733
SamuelPazz <samuel.paz@sptech.school>
2024-04-20 12:07:37 -0300

    botões indo para outras paginas

--- front/Login-Registro/script.js
+++ front/Login-Registro/script.js
@@ -1,5 +1,5 @@
 
-function cadastrar(){
+function cadastrarempresa(){
     var razao = inputRazao.value;
     var cnpj = inputCNPJ.value;
     var nomeRepresentante = inputRepresentante.value;
@@ -7,41 +7,28 @@ function cadastrar(){
     var telefoneEmpresa = inputTelefone.value;
     var senhaEmpresa = inputSenha.value;
     var confirmEmpresa = inputConfirmarSenha.value;
-    var indice_arroba = email.indexOf('@')
+    var indice_arroba = emailEmpresa.indexOf('@')
    
-    if(indice_arroba < 0){
-        document.getElementById("spanEmail").style.display = "block";
+    if(indice_arroba >= 0 && senhaEmpresa.length >= 8 && confirmEmpresa == senhaEmpresa){
+        spanEmail.innerHTML = ``;
+        spanSenha.innerHTML = ``;
+        spanConfirmarSenha.innerHTML = ``;
+        spanRealizado.innerHTML = `Cadastro Realizado`
+    } if(indice_arroba < 0){
         spanEmail.innerHTML=`Insira um email valido`
-    } else {
-        document.getElementById("spanEmail").style.display = "none"; 
-    }
-
-    if(senhaEmpresa.length < 8){
-        document.getElementById("spanSenha").style.display = "block";
+    } if(senhaEmpresa.length < 8){
         spanSenha.innerHTML=`Senha fraca, insira no minímo de 8 digitos`
-    } else {
-        document.getElementById("spanSenha").style.display = "none"; 
-    }
-
-    if(confirmEmpresa != senhaEmpresa){
-        document.getElementById("spanConfirmarSenha").style.display = "block";
+    } if(confirmEmpresa != senhaEmpresa){
         spanConfirmarSenha.innerHTML=`A confirmação de senha não corresponde`
-    } else {
-        document.getElementById("spanConfirmarSenha").style.display = "none"; 
-    }
-
-    if(indice_arroba > 0 && senhaEmpresa.length >= 8 && confirmEmpresa == senhaEmpresa){
-        document.getElementById("spanRealizado").style.color = "#00FF00"
-        spanRealizado.innerHTML= `Cadastro Realizado com sucesso`
-    }
+    } 
 }
 
-function spanEntrar(){
+function entrar(){
     var email = inputEmail.value;
     var senha = inputSenha.value;
 
     if(email == '@empresa' && senha == 'empresa123' || (email == '@func' && senha == 'func123')){
-        document.getElementById("spanEntrar").style.color = "#00FF00";
+        spanEntrar.style.color = "#00FF00";
         spanEntrar.innerHTML=`Login realizado com sucesso`
     } else {
         document.getElementById("spanEntrar").style.color = "#FF0000";
@@ -59,30 +46,17 @@ function cadastrarfunc(){
     var confirmFunc = inputConfirmFunc.value;
     var indice_arroba = emailFunc.indexOf('@');
 
-     if(indice_arroba < 0){
-        document.getElementById("spanEmail").style.display = "block";
+
+    if(indice_arroba >= 0 && senhaFunc.length >= 8 && confirmFunc == senhaFunc){
+        spanEmail.innerHTML = ``;
+        spanSenha.innerHTML = ``;
+        spanConfirmarSenha.innerHTML = ``;
+        spanRealizado.innerHTML = `Cadastro Realizado`
+    } if(indice_arroba < 0){
         spanEmail.innerHTML=`Insira um email valido`
-    } else {
-        document.getElementById("spanEmail").style.display = "none"; 
-    }
-
-    if(senhaFunc.length < 8){
-        document.getElementById("spanSenha").style.display = "block";
+    } if(senhaFunc.length < 8){
         spanSenha.innerHTML=`Senha fraca, insira no minímo de 8 digitos`
-    } else {
-        document.getElementById("spanSenha").style.display = "none"; 
-    }
-
-    if(confirmFunc != senhaFunc){
-        document.getElementById("spanConfirmarSenha").style.display = "block";
+    } if(confirmFunc != senhaFunc){
         spanConfirmarSenha.innerHTML=`A confirmação de senha não corresponde`
-    } else {
-        document.getElementById("spanConfirmarSenha").style.display = "none"; 
-    }
-
-    if(indice_arroba > 0 && senhaFunc.length >= 8 && confirmFunc == senhaFunc){
-        document.getElementById("spanRealizado").style.color = "#00FF00"
-        spanRealizado.innerHTML= `Cadastro Realizado com sucesso`
-    }
-    
+    } 
 }
